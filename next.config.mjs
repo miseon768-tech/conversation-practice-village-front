1d37a7ee2e1c3e13389f3fd9951ac8a978e77719
miseon768-tech <miseon768@gmail.com>
2026-03-04 17:18:40 +0900

    Dockerfile 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Docker standalone 출력 모드 활성화
-    output: 'standalone',
-
     async rewrites() {
-        const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
         return [
             {
                 source: '/api/:path*',
-                destination: `${backendUrl}/api/:path*`, // Spring Boot 주소
+                destination: 'http://localhost:8080/api/:path*', // Spring Boot 주소
             },
         ];
     },
